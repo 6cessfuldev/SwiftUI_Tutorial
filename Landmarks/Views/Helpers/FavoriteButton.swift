b19e2bb788cbb68e709ab1e3e84bc69c1171c16b
6cessfuldev <6cessfuldev@gmail.com>
2024-07-06 16:08:30 +0900

    handling user input

---
+++ Landmarks/Views/Helpers/FavoriteButton.swift
@@ -0,0 +1,26 @@
+//
+//  FavoriteButtn.swift
+//  Landmarks
+//
+//  Created by 육성민 on 7/6/24.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundStyle(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+#Preview {
+    FavoriteButton(isSet: .constant(true))
+}
